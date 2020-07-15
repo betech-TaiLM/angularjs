20ae9ea77af342329f7c62c69803af5c202454c3
tsunakun <tailm@bisync.jp>
2020-07-15 08:33:48 +0700

    first_upload

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar">
+    <a href="#" class="sidebar-toggler">
+        <i data-feather="menu"></i>
+    </a>
+    <div class="navbar-content">
+        <form class="search-form">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <div class="input-group-text">
+                        <i data-feather="search"></i>
+                    </div>
+                </div>
+                <input type="text" class="form-control" id="navbarForm" placeholder="Search here...">
+            </div>
+        </form>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a href=""></a>
+            </li>
+            <li class="nav-item">
+                English
+            </li>
+            <li class="nav-item">
+                English
+            </li>
+        </ul>
+    </div>
+</nav>
